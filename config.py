e1615849d69c7fd5cc1ff1e3a939bc35c27b05e3
Daniel BOKINGO <bokingodaniel4@gmail.com>
2026-06-05 03:21:49 +0200

    delete/.env

--- config.py
+++ config.py
@@ -1,11 +1,15 @@
 import os 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', 'une_cle_tres_secrete_et_longue_12345')
 
-    MYSQL_HOST = 'localhost'
-    MYSQL_USER = 'root'
-    MYSQL_PASSWORD = 'Daniel12349'
-    MYSQL_DB = 'elikya_db'
+    MYSQL_HOST = os.getenv('DB_HOST')
+    MYSQL_USER = os.getenv('DB_USER')
+    MYSQL_PASSWORD = os.getenv('DB_PASSWORD')
+    MYSQL_DB = os.getenv('DB_NAME')
     MYSQL_CURSORCLASS = 'DictCursor'
