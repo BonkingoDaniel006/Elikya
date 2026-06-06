89574bfb2e7442869e45be7c99a40497c3f1c008
Daniel BOKINGO <bokingodaniel4@gmail.com>
2026-06-06 23:08:26 +0200

    feature:buyer-profil

--- feed/routes.py
+++ feed/routes.py
@@ -1,9 +1,2 @@
 from flask import Blueprint, render_template, redirect, url_for, flash, request, session, current_app
 from feed.models import Produits
-
-
-
-produits_bp = Blueprint('produits', __name__)
-
-
-@produits_bp.route('/')
---
+++ profils/routes.py
@@ -0,0 +1,17 @@
+from flask import Blueprint, render_template, redirect, url_for, flash, request, session, current_app
+from flask_login import login_user, logout_user, login_required, current_user
+from ext import bcrypt, mail
+from profils.models import Buyer
+
+buyer_bp = Blueprint('buyer', __name__)
+
+@buyer_bp.route("/profil_acheteur")
+@login_required
+def profil_acheteur():
+    # Récupération des items du panier et du total via la méthode de classe
+    cart_items, total = Buyer.get_panier(current_user.id)
+    
+    # Utilisation des informations de l'utilisateur connecté (UserMixin)
+    user_info = current_user.get_claims()
+    
+    return render_template("profil_acheteur.html", user=user_info, cart_items=cart_items, cart_total=total)
