77fec88108be2dd3971cab6db0e05f18329f4fb5
Daniel BOKINGO <bokingodaniel4@gmail.com>
2026-06-06 18:42:16 +0200

    fix/requirements.txt-bug

--- ext.py
+++ ext.py
@@ -1,11 +1,13 @@
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
 from flask_mail import Mail
+from flask_wtf.csrf import CSRFProtect
 
 # Initialisation à vide des extensions (pattern factory)
 bcrypt = Bcrypt()
 login_manager = LoginManager()
 mail = Mail()
+csrf = CSRFProtect()
 
 # Redirection automatique si un utilisateur non connecté tente d'accéder à une page protégée
 login_manager.login_view = 'auth.connexion'
