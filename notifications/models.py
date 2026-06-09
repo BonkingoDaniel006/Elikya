673674da52828bd045fa7cc3e9e4f13a2e77ee2f
Daniel BOKINGO <bokingodaniel4@gmail.com>
2026-06-09 12:56:03 +0200

    feat:add-notif-page

--- notifications/models.py
+++ notifications/models.py
@@ -1,5 +1,16 @@
 from ext import get_db_connection
 
-class notifications():
-    def _init__(self):
+class Notification():
+    def __init__(self):
         pass
+    def get_claims(self):
+        pass
+    @classmethod
+    def get_notifications(cls, user_id):
+        conn = get_db_connection()
+        cursor = conn.cursor(dictionary=True)
+        cursor.execute("SELECT * FROM notifications WHERE user_id = %s", (user_id,))
+        notifications = cursor.fetchall()
+        cursor.close()
+        conn.close()
+        return notifications
